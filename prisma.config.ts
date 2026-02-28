20775e8721948b53f5ec9224deb49d3c359f1864
jsdevrazuislam <razuislam.upwork@gmail.com>
2026-02-28 20:15:34 +0600

    fix: deploy issue

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,10 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from '@prisma/config';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 export default defineConfig({
     datasource: {
-        url: env("DATABASE_URL"),
+        url: process.env.DATABASE_URL,
     },
 });
